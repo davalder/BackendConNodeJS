6a436e4a3a521e9726609bf9401769869193f9d0
davalder <davalder@hotmail.com>
2021-05-19 16:42:58 -0500

    clase7, captura de eventos con promesas

--- arquitectura-node/promise.js
+++ arquitectura-node/promise.js
@@ -1,9 +1,15 @@
-const asyncCallback = function (cb) {
+
+const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
         if (Math.random() < 0.5) {
-            return cb(null, 'hello world');
+            resolve('hello world');
         } else {
-            cb(new Error('hello error'))
+            reject(new Error('hello error'))
         }
     }, 2000);
-};
+});
+
+promise
+    .then(msg => msg.toUpperCase())
+    .then(msg => console.log('message', msg))
+    .catch(err => console.log('error', err));
