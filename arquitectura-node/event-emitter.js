f2128a1ee07e3e4a348ed1d0591e77e38488160f
davalder <davalder@hotmail.com>
2021-05-20 10:19:59 -0500

    clase7, prueba agregando otro logger.on, pero no en orden, la impresion conserva el orden gracias al emmitter

--- arquitectura-node/event-emitter.js
+++ arquitectura-node/event-emitter.js
@@ -15,6 +15,7 @@ const logger = new Logger();
 logger.on('start', () => console.log('Starting'));
 logger.on('finish', () => console.log('Finishing'));
 logger.on('finish', () => console.log('It\'s Done'));
+logger.on('start', () => console.log('Starting2')); //en la impresion se conserva el orden del emitter
 
 logger.execute(() => console.log('Hello world'));  //
-logger.execute(() => setTimeout(() => console.log('hello world'), 1000)); // si ejecutamos codifo asincrono el orden no permanece
+//logger.execute(() => setTimeout(() => console.log('hello world'), 1000)); // si ejecutamos codifo asincrono el orden no permanece
