165dfb6553c413cad14ab219efe81f4eb3bad272
davalder <davalder@hotmail.com>
2021-05-27 18:38:25 -0500

    clase22, metodos idempotentes del CRUD

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -18,6 +18,62 @@ function moviesApi(app) {
             next(err);
         }
     });
+
+
+    router.get("/:movieId", async function (req, res, next) {
+        try {
+            const movies = await Promise.resolve(moviesMock[0]);
+
+            res.status(200).json({
+                data: movies,
+                message: 'Movie retrieved'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+
+    router.post("/", async function (req, res, next) {
+        try {
+            const createdMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(201).json({
+                data: createdMovieId,
+                message: 'Movie created'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+
+    router.put("/:movieId", async function (req, res, next) {
+        try {
+            const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: updatedMovieId,
+                message: 'Movie updated'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+
+    router.delete("/:movieId", async function (req, res, next) {
+        try {
+            const deletedMovieId = await Promise.resolve(moviesMock[0].id);
+
+            res.status(200).json({
+                data: deletedMovieId,
+                message: 'Movie deleted'
+            });
+        } catch (err) {
+            next(err);
+        }
+    });
 }
 
 module.exports = moviesApi;
